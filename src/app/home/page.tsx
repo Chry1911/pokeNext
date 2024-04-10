04582336479c3f55655c33f50a8af2a4837f6958
= <=>
2024-04-10 20:22:08 +0200

    added second page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+
+export default function HomeDashBoard() {
+  return (
+    <div className="container mx-auto py-10">
+        <h1 className="text-2xl">Home DashBoard</h1>
+    </div>
+  );
+}
